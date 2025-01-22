a29b70b496bedc0feca9356345ceff6e86d82f36
Piero Nicolini <naturalnen@gmail.com>
2025-01-22 12:11:50 -0300

    finish conversor de monedas

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,6 @@ let options = document.querySelectorAll('option')
 let valorElegido = 0;
 let opcionElegida = 'dolar';
 
-
 options.forEach((option, i)=> {
   option.id = option.value;
 })
@@ -14,31 +13,41 @@ input.addEventListener('change', () => actualizarElegido())
 
 seleccion.addEventListener('change', () => actualizarElegido())
 
-
 function actualizarElegido(){
   opcionElegida = seleccion.value;
   valorElegido = Number(input.value);
   convertirMoneda(valorElegido, opcionElegida)
 }
 
-function convertirMoneda(valor, divisa){
-  
-  resultado.innerText = `${valor}, ${divisa}`
-}
-
-
-
-
 async function getMonedas() {
   const endpoint = "https://mindicador.cl/api/";
   const res = await fetch(endpoint);
   const monedas = await res.json();
-  console.log(Object.values(monedas) //convertimos objeto en array
+  const monedasFiltradasPorPesos = Object.values(monedas) //convertimos objeto en array
   .filter(elemento=> typeof elemento === 'object') // filtramos por tipo objeto
-  .filter(elemento=>elemento.unidad_medida == 'Pesos')) // filtramos por unidad_medida
+  .filter(elemento=>elemento.unidad_medida == 'Pesos'); // filtramos por unidad_medida
+   return monedasFiltradasPorPesos;
 }
 
-getMonedas()
+async function convertirMoneda(valor, divisa){
+  const monedasFiltradasPorPesos = await getMonedas();
+  let simbol = '';
+  let divisaFiltrada = monedasFiltradasPorPesos.filter(elemento=> elemento.codigo== divisa);
+  let conversion = valor / divisaFiltrada[0].valor;
+
+  if (divisa == 'dolar'){
+    simbol = "US$ "
+  } else if (divisa == 'euro') {
+    simbol = "€ "
+  } else {
+    simbol = `${divisa}'s `.toUpperCase()
+  }
+
+  resultado.innerText = `${simbol}${conversion.toFixed(2)}`;
+}
+
+actualizarElegido()
+
 
 //   function prepararConfiguracionParaLaGrafica(monedas) {
 //     // Creamos las variables necesarias para el objeto de configuración
